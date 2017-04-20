970b3c2aa3615c20aef8bd54403dabe87a37478f
Rajesh Kumar <ramrajesh0205@users.noreply.github.com>
2017-04-20 19:50:02 +0530

    Replacing local links with CDN links
    Replacing local links with CDN links

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<!-- Tell the browser to be responsive to screen width -->
 		<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 		<!-- Bootstrap 3.3.6 -->
-		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css">
 		<!-- Font Awesome -->
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
 		<!-- Ionicons -->
@@ -15,7 +15,7 @@
 		<!-- Theme style -->
 		<link rel="stylesheet" href="dist/css/SPI.min.css">
 		<!-- iCheck -->
-		<link rel="stylesheet" href="plugins/iCheck/square/blue.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.1/skins/square/blue.css">
 
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -164,9 +164,9 @@
 	</div>
 	
 		<!-- jQuery 2.2.3 -->
-		<script src="plugins/jQuery/jquery-2.2.3.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
 		<!-- Bootstrap 3.3.6 -->
-		<script src="bootstrap/js/bootstrap.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>
 		<script>
 			$(document).ready(function() {
 				$("#login").submit(function(e) {
